ae4e48b30ae1644d41c94a10e05ec4dd0cf0f91b
iTzSebasVFX <iTzSebasVFX>
2025-03-27 06:23:48 -0500

    Cambio en archivo de requerimientos

--- WEBBASIC/app.py
+++ WEBBASIC/app.py
@@ -1,5 +1,10 @@
 from flask import Flask, redirect, render_template, request, url_for
 import csv
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -15,21 +20,45 @@ def Persona():
 def enviar_datos():
   nombre = request.form['nombre']
   apellido = request.form['apellido']
-  ocio = request.form['social']
-  personal = request.form['espiritual']
-  dinero = request.form['exito']
-  trabajo = request.form['familia']
-  fisica = request.form['profesion']
-  familiar = request.form['fisico']
-  social = request.form['financiero']
-  espiritual = request.form['crecimiento']
+  ocio = (int)(request.form['social'])
+  personal = (int)(request.form['espiritual'])
+  dinero = (int)(request.form['exito'])
+  trabajo = (int)(request.form['familia'])
+  fisica = (int)(request.form['profesion'])
+  familiar = (int)(request.form['fisico'])
+  social = (int)(request.form['financiero'])
+  espiritual = (int)(request.form['crecimiento'])
 
   # Procesar los datos (por ejemplo, guardarlos en una base de datos)
   # Aquí simplemente los imprimimos por el momento
   print(f'Nombre: {nombre}')
 
   totalMavIa = [nombre, apellido, ocio, personal, dinero, trabajo, fisica, familiar, social, espiritual]
-
+  Promedio = {
+    'ocio': [ocio], 
+    'personal': [personal], 
+    'dinero': [dinero], 
+    'trabajo': [trabajo],
+    'fisica': [fisica],
+    'familiar': [familiar],
+    'social': [social],
+    'espiritual': [espiritual]
+  }
+  df = pd.DataFrame(Promedio)
+  
+  Total_mean = np.mean(df[['ocio', 'personal', 'dinero', 'trabajo', 'fisica', 'familiar', 'social', 'espiritual']])
+  print(Total_mean)
+  
+  areas = ['ocio', 'personal', 'dinero', 'trabajo', 'fisica', 'familiar', 'social', 'espiritual']
+  total = [ocio, personal, dinero, trabajo, fisica, familiar, social, espiritual]
+  plt.bar(areas, total, color='cyan')
+  plt.title(f'Balance de rueda de la vida de: {nombre}')
+  plt.xlabel('Area')
+  plt.ylabel('Altura')
+  # Guardar la imagen en un archivo
+  plt.savefig('static/uploads/balance.png')  # Guarda en la carpeta 'static'
+  plt.close()  # Cierra la figura para liberar memoria
+  
   with open("datos.csv", "a+", newline ='') as csvfile:
     wr = csv.writer(csvfile, dialect='excel', delimiter=',')
     wr.writerow(totalMavIa)
